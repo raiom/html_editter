b0e0f13b2ca6c97383ec3ed6d545ba81971f55d6
unknown <ht12a019@ht12a019pc.cs.osakac.ac.jp>
2014-10-16 14:17:20 +0900

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# adding this fixes the issue
+#require 'tzinfo'
+
+# Initialize the Rails application.
+#HtmlEditter::Application.initialize!
+Rails.application.initialize!
